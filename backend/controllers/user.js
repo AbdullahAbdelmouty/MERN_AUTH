108caba205415b1da143597011143c466c61202c
Abdullah <abdallah0115019@gmail.com>
2023-07-23 15:15:10 +0300

    hashing password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,13 @@
 const User = require('../models/user')
 
 const signup =  async (req, res) => {
-    res.send('signup')
+    const {email, password} = req.body
+    try {
+        User.signup(email, password)
+        res.status(201).json({message: 'User created', user: {email: email, password: password}})
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
 }
 
 const login = async (req, res) => {
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcryptjs')
 const UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -10,5 +10,18 @@ const UserSchema = new mongoose.Schema({
         required: true
     }
 })
+
+// static signup method
+UserSchema.statics.signup = async function(email, password) {
+    const exists = await this.findOne({ email })
+    if (exists) {
+      throw Error('Email already in use')
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password,salt)
+    const user = this.create({email, password: hash})
+    return user
+}
+
 
 module.exports = mongoose.model("User",UserSchema);
