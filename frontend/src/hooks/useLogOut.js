21045ea7e393a7ad4ff71485b01e7db76fad3371
Abdullah <abdallah0115019@gmail.com>
2023-08-04 20:43:33 +0300

    handle error in the last commit

--- frontend/src/hooks/useLogOut.js
+++ frontend/src/hooks/useLogOut.js
@@ -1,12 +1,13 @@
 import { AuthContext } from "../context/AuthContext";
+import { useWorkoutsContext } from "./useWorkoutsContext";
 import { useContext } from "react";
-import {dispatchWorkouts} from '../context/WorkoutsContext'
 export const useLogOut = () => {
     const { dispatch } = useContext(AuthContext);
+    const { dispatch: dispatchWorkouts } = useWorkoutsContext();
     const logOut = ()=> {
         localStorage.removeItem('user');
         dispatch({type:'LOGOUT'});
-        dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null })
+        dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null });
     }
     
     return {logOut}
