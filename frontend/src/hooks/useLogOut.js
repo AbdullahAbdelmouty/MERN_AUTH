276ff4756385affc29aa1489050482da066b03c1
Abdullah <abdallah0115019@gmail.com>
2023-08-04 20:37:47 +0300

    update sign up and logout

--- frontend/src/hooks/useLogOut.js
+++ frontend/src/hooks/useLogOut.js
@@ -1,10 +1,12 @@
 import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
+import {dispatchWorkouts} from '../context/WorkoutsContext'
 export const useLogOut = () => {
     const { dispatch } = useContext(AuthContext);
     const logOut = ()=> {
         localStorage.removeItem('user');
         dispatch({type:'LOGOUT'});
+        dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null })
     }
     
     return {logOut}
